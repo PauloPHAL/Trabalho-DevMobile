e156c2544f90f19f76da7dfb20b092c7865209ee
Paulo Henrique <henriqueloss@hotmail.com>
2023-11-15 20:07:41 -0300

    Quase Pronto

--- flutter_delivery/lib/pages/cadastro.page.dart
+++ flutter_delivery/lib/pages/cadastro.page.dart
@@ -75,148 +75,150 @@ class _CadastroPageState extends State<CadastroPage> {
           ],
         ),
       ),
-      body: Center(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color(0xFF8A2387),
-                Color(0xFFE94057),
-                Color(0xFFF27121)
-              ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFF8A2387),
+                  Color(0xFFE94057),
+                  Color(0xFFF27121)
+                ],
+              ),
             ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 70),
-              Container(
-                height: 580,
-                width: 345,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 250,
-                      child: TextField(
-                        controller: nomeController,
-                        decoration: const InputDecoration(
-                          labelText: "Nome",
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 250,
-                      child: TextField(
-                        controller: telefoneController,
-                        decoration: const InputDecoration(
-                          labelText: "Telefone",
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 250,
-                      child: TextField(
-                        controller: enderecoController,
-                        decoration: const InputDecoration(
-                          labelText: "Endereço",
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 250,
-                      child: TextField(
-                        controller: cepController,
-                        decoration: const InputDecoration(
-                          labelText: "CEP",
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 250,
-                      child: TextField(
-                        controller: numeroCasaController,
-                        decoration: const InputDecoration(
-                          labelText: "Número",
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 250,
-                      child: TextField(
-                        controller: emailController,
-                        decoration: const InputDecoration(
-                          labelText: "Email",
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 250,
-                      child: TextField(
-                        controller: senhaController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          labelText: "Senha",
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 250,
-                      child: TextField(
-                        controller: confirmarSenhaController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          labelText: "Confirmar senha",
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        _cadastrar();
-                      },
-                      child: Container(
-                        alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 70),
+                Container(
+                  height: 580,
+                  width: 345,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
                         width: 250,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          gradient: const LinearGradient(
-                            colors: [
-                              Color(0xFFE94057),
-                              Color(0xFFF27121),
-                            ],
+                        child: TextField(
+                          controller: nomeController,
+                          decoration: const InputDecoration(
+                            labelText: "Nome",
                           ),
                         ),
-                        child: const Padding(
-                          padding: EdgeInsets.all(12.0),
-                          child: Text(
-                            'Cadastrar',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
+                      ),
+                      SizedBox(
+                        width: 250,
+                        child: TextField(
+                          controller: telefoneController,
+                          decoration: const InputDecoration(
+                            labelText: "Telefone",
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 250,
+                        child: TextField(
+                          controller: enderecoController,
+                          decoration: const InputDecoration(
+                            labelText: "Endereço",
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 250,
+                        child: TextField(
+                          controller: cepController,
+                          decoration: const InputDecoration(
+                            labelText: "CEP",
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 250,
+                        child: TextField(
+                          controller: numeroCasaController,
+                          decoration: const InputDecoration(
+                            labelText: "Número",
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 250,
+                        child: TextField(
+                          controller: emailController,
+                          decoration: const InputDecoration(
+                            labelText: "Email",
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 250,
+                        child: TextField(
+                          controller: senhaController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            labelText: "Senha",
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 250,
+                        child: TextField(
+                          controller: confirmarSenhaController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            labelText: "Confirmar senha",
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          _cadastrar();
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 250,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            gradient: const LinearGradient(
+                              colors: [
+                                Color(0xFFE94057),
+                                Color(0xFFF27121),
+                              ],
+                            ),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(12.0),
+                            child: Text(
+                              'Cadastrar',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                  ],
+                      const SizedBox(
+                        height: 30,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- flutter_delivery/lib/pages/login.page.dart
+++ flutter_delivery/lib/pages/login.page.dart
@@ -61,158 +61,164 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-       child: Container(
-         height: MediaQuery.of(context).size.height,
-         width: MediaQuery.of(context).size.width,
-         decoration: const BoxDecoration(
-           gradient: LinearGradient(
-             begin: Alignment.topLeft,
-             end: Alignment.bottomRight,
-             colors: [
-               Color(0xFF8A2387),
-               Color(0xFFE94057),
-               Color(0xFFF27121)
-             ]
-           )
-         ),
-         child: Column(
-           crossAxisAlignment: CrossAxisAlignment.center,
-           children: [
-             const SizedBox(height: 80,),
-             SizedBox(
-               width: 50, // Largura desejada
-               height: 50, // Altura desejada
-               child: Image.asset('images/logo.png'),
-             ),
-             const SizedBox(height: 15,),
-             const Text(
-               "Flutter Delivery",
-                style: TextStyle(
-                 color: Colors.black, // Remova o segundo "TextStyle"
-                 fontSize: 20,
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFF8A2387),
+                  Color(0xFFE94057),
+                  Color(0xFFF27121)
+                ],
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 80),
+                SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: Image.asset('images/logo.png'),
                 ),
-             ),
-             const SizedBox(height: 30),
-             Container(
-               height: 480,
-               width: 345,
-               decoration: BoxDecoration(
-                   color: Colors.white,
-                    borderRadius: BorderRadius.circular(10)
-               ),
-               child: Column(
-                 crossAxisAlignment: CrossAxisAlignment.center,
-                 children: [
-                   const SizedBox(height: 30,),
-                   const Text('Olá',
-                   style: TextStyle(
-                     fontSize: 35,
-                     fontWeight: FontWeight.bold
-                   ),
-                   ),
-                   const SizedBox(height: 10, ),
-                   const Text("Acesse sua conta realizando o login",
-                     style: TextStyle(
-                       fontSize: 15,
-                       color: Colors.grey,
-                     ),
-                   ),
-                  const SizedBox(height: 20,),
-                   SizedBox(
-                     width: 250,
-                     child: TextField(
-                       controller: emailController,
-                       decoration: const InputDecoration(
-                         labelText: "Email",
-                       ),
-                     ),
-                   ),
-                   SizedBox(
-                     width: 250,
-                     child: TextField(
-                       controller: senhaController,
-                       obscureText: true,
-                       decoration: const InputDecoration(
-                         labelText: "Senha",
-                       ),
-                     ),
-                   ),
-                   const Padding(
-                     padding: EdgeInsets.fromLTRB(20, 20, 40, 20),
-                     child: Row(
-                       mainAxisAlignment: MainAxisAlignment.end,
-                         children: [
-                           Text("Esqueceu a Senha?",
-                             style: TextStyle(
-                               color: Colors.deepOrange,
-                             ),
-                           )
-                         ],
-                     ),
-                   ),
-                   const SizedBox(height: 20,),
-                   GestureDetector(
-                     onTap: () {
-                       _login();
-                     },
-                     child: Container(
-                       alignment: Alignment.center,
-                       width: 250,
-                       decoration: BoxDecoration(
-                         borderRadius: BorderRadius.circular(50),
-
-                         gradient: const LinearGradient(
-                             colors: [
-                               Color(0xFFE94057),
-                               Color(0xFFF27121)
-                             ] )
-                       ),
-                       child: const Padding(
-                         padding: EdgeInsets.all(12.0),
-                         child: Text('Login',
-                         style: TextStyle(
-                           color: Colors.white,
-                           fontSize: 20,
-                           fontWeight: FontWeight.bold
-                         ),),
-                       ),
-                     ),
-                   ),
-                   const SizedBox(
-                     height: 30,
-                   ),
-                   GestureDetector(
-                     onTap: () {
-
-                       Navigator.push(
-                         context,
-                         MaterialPageRoute(builder: (context) => CadastroPage()), // Use a página de CadastroPage
-                       );
-                     },
-                     child: const Text(
-                       "Cadastrar",
-                       style: TextStyle(
-                         fontWeight: FontWeight.bold,
-                         color: Color(0xFFE94057),
-                       ),
-                     ),
-                   ),
-                   const SizedBox(
-                     height: 30,
-                   ),
-
-                 ],
-               ),
-             )
-           ],
-         ),
-       )
+                SizedBox(height: 15),
+                const Text(
+                  "Flutter Delivery",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 30),
+                Container(
+                  height: 480,
+                  width: 345,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 30),
+                      const Text(
+                        'Olá',
+                        style: TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        "Acesse sua conta realizando o login",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: 250,
+                        child: TextField(
+                          controller: emailController,
+                          decoration: const InputDecoration(
+                            labelText: "Email",
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 250,
+                        child: TextField(
+                          controller: senhaController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            labelText: "Senha",
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(20, 20, 40, 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "Esqueceu a Senha?",
+                              style: TextStyle(
+                                color: Colors.deepOrange,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      GestureDetector(
+                        onTap: () {
+                          _login();
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 250,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            gradient: const LinearGradient(
+                              colors: [
+                                Color(0xFFE94057),
+                                Color(0xFFF27121),
+                              ],
+                            ),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(12.0),
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => CadastroPage(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          "Cadastrar",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFE94057),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
