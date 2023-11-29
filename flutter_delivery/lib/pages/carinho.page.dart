95aef6a3f0fd8f8c33be7f77f207fca581299f18
Pedro Henrique de Rezende Silva <102803305+PedroRezende73@users.noreply.github.com>
2023-11-29 20:18:35 -0300

    carrinho feito direito pq o paulo n fez

--- flutter_delivery/lib/pages/carinho.page.dart
+++ flutter_delivery/lib/pages/carinho.page.dart
@@ -82,7 +82,7 @@ class _CarrinhoPageState extends State<CarrinhoPage> {
   Future<void> _realizarPedido(
       double totalPedido, String IDs, String quant) async {
     String url = 'https://dev.levsistemas.com.br/api.flutter/pedidos';
-    print(totalPedido);
+    print(totalPedido.toString());
     try {
       final response = await http.post(
         Uri.parse(url),
@@ -96,6 +96,8 @@ class _CarrinhoPageState extends State<CarrinhoPage> {
         },
       );
       if (response.statusCode == 200) {
+        carrinho = [];
+        setState(() {});
         _onAcerto();
       } else {
         _onError();
